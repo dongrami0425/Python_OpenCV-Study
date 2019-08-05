b85866ab58cd03f69d0407828306fbc5f76f9124
dongmin lee <ldm8191@outlook.kr>
2019-08-05 14:30:07 +0900

    190804_Jumptopython_Organize_up to_Exception

---
+++ opentutorials_python2/JumptoPython/Part_5_Python_Wing/game/graphic/render.py
@@ -0,0 +1,14 @@
+'''
+# render.py
+from game.sound.echo import echo_test
+def render_test():
+    print ("render")
+    echo_test()
+'''
+
+# render.py
+from ..sound.echo import echo_test
+
+def render_test():
+    print ("render")
+    echo_test()
