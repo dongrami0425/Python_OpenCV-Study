52a4bf5a61afb8221e1bb6b2fa4757f64043d3f0
dongmin lee <ldm8191@outlook.kr>
2019-10-28 17:47:36 +0900

    191020_191028 opencv_python_from to TrackerAPIs to K-means cluster

--- opencv_project_python-master/opencv_project_python-master/09.ml/k-means_color.py
+++ opencv_project_python-master/opencv_project_python-master/09.ml/k-means_color.py
@@ -1,10 +1,29 @@
+# 16만 가지 색상의 컬러 영상을 16가지 색상으로 군집화하는 예제
+# 영상은 하나의 색상을 위해 24비트(8비트 X 3)가 필요하고 약 16만가지의 생각을 표현가능 => 미세한 차이의 색상을 그룹 지어서 같은 색상으로 처리하면 용량을 줄일수 있다.
 import numpy as np
 import cv2
 
 K = 16 # 군집화 갯수(16컬러) ---①
-img = cv2.imread('../img/taekwonv1.jpg')
+img = cv2.imread('img/taekwonv1.jpg')
+print('img :',img,'img.shape',np.shape(img))
+'''
+img.shape : (444,400,3)
+'''
 # 군집화를 위한 데이타 구조와 형식 변환 ---②
 data = img.reshape((-1,3)).astype(np.float32)
+print('data :', data)
+print('data.shape :',np.shape(data))
+'''
+data [[176. 181. 180.]
+ [176. 181. 180.]
+ [176. 181. 180.]
+ ...
+ [213. 218. 221.]
+ [214. 219. 222.]
+ [214. 219. 222.]]
+data.shape : (177600, 3)
+'''
+
 # 반복 중지 요건 ---③
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 # 평균 클러스터링 적용 ---④
--- opencv_project_python-master/opencv_project_python-master/09.ml/k-means_random.py
+++ opencv_project_python-master/opencv_project_python-master/09.ml/k-means_random.py
@@ -11,14 +11,21 @@ data = np.vstack((a,b)).astype(np.float32)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 # 평균 클러스터링 적용 ---⑤
 ret,label,center=cv2.kmeans(data,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+#print('label : ',label,'data',data)
+
+
 # label에 따라 결과 분류 ---⑥
 red = data[label.ravel()==0]
 blue = data[label.ravel()==1]
+print('red :',red,'blue :',blue)
+
 
 # plot에 결과 출력 ---⑦
 plt.scatter(red[:,0],red[:,1], c='r')
 plt.scatter(blue[:,0],blue[:,1], c='b')
 # 각 그룹의 중앙점 출력 ---⑧
+print('center :',center)
 plt.scatter(center[0,0],center[0,1], s=100, c='r', marker='s')
 plt.scatter(center[1,0],center[1,1], s=100, c='b', marker='s')
 plt.show()
